17b0894a3cf86ebad130a69648ab49675fbffc89
Poonam Dahiya <poonam.dahiya@applitools.com>
2021-11-01 11:27:17 +1100

    test

--- applitools.config.js
+++ applitools.config.js
@@ -1,13 +1,13 @@
 module.exports = {
     testConcurrency: 20,
     apiKey: process.env.APPLITOOLS_API_KEY,
-    batchId: process.env.APPLITOOLS_BATCH_ID
+    batchId: process.env.APPLITOOLS_BATCH_ID,
     batchName:'ANZDemo',
-     showLogs: true,
-     notifyOnCompletion: true,
-     batchSequenceName:'Test Batch',
-     accessibilityValidation: {level: 'AA', guidelinesVersion: 'WCAG_2_0'},
-     browser: [
+    showLogs: true,
+    notifyOnCompletion: true,
+    batchSequenceName:'Test Batch',
+    accessibilityValidation: {level: 'AA', guidelinesVersion: 'WCAG_2_0'},
+    browser: [
         // Add browsers with different viewports
         {width: 1024, height: 768, name: 'chrome'},
         {width: 1366, height: 768, name: 'chrome'},
