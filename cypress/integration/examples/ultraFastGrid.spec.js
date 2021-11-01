6338786db3913a73c1bdad170ebb64df14d94342
Poonam Dahiya <poonam.dahiya@applitools.com>
2021-11-01 11:19:01 +1100

    test

--- cypress/integration/examples/ultraFastGrid.spec.js
+++ cypress/integration/examples/ultraFastGrid.spec.js
@@ -1,33 +1,28 @@
 describe("AppTest", () => {
 
-    it(`UltraFast Test`, function () {
+    it(`ultraFastTest`, function () {
         // Navigate to the url we want to test
         // ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
         // but then change the above URL to https://demo.applitools.com/index_v2.html
         // (for the 2nd run)
         cy.visit('https://demo.applitools.com');
 
-
-	cy.eyesOpen({
-                 appName: 'Demo App - Cypress',
-                 testName: 'Smoke Test - Cypress',
-          })
-
-
-		cy.get ('#log-in', { timeout: 20000 }).should('be.visible').then(()=>{
-	        cy.eyesCheckWindow({
-                tag: "Login Window",
-                target: 'window',
-                fully: true
-            });
-		    })
+        // Call Open on eyes to initialize a test session
+        cy.eyesOpen({
+            appName: 'Demo App',
+            testName: 'Ultrafast grid demo',
+        })
 
         // check the login page with fluent api, see more info here
         // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
+        cy.eyesCheckWindow({
+            tag: "Login Window",
+            target: 'window',
+            fully: true
+        });
 
+        cy.get('#log-in').click()
 
-        cy.get('#log-in').click().as('firstNav')
-		cy.wait('@firstNav');
         // Check the app page
         cy.eyesCheckWindow({
             tag: "App Window",
