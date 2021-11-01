5b07d6d8bc9c59876748a1f71ffef294aeda505c
Poonam Dahiya <poonam.dahiya@applitools.com>
2021-11-01 12:26:01 +1100

    test

--- cypress/integration/examples/ultraFastGrid.spec.js
+++ cypress/integration/examples/ultraFastGrid.spec.js
@@ -5,7 +5,7 @@ describe("AppTest", () => {
            // ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
            // but then change the above URL to https://demo.applitools.com/index_v2.html
            // (for the 2nd run)
-           cy.visit('https://demo.applitools.com/index_v2.html');
+           cy.visit('https://demo.applitools.com/');
 
            // Call Open on eyes to initialize a test session
            cy.eyesOpen({
@@ -122,4 +122,31 @@ describe("AppTest", () => {
             // Call Close on eyes to let the server know it should display the results
             cy.eyesClose()
         });
+
+    it(`DemoApp_Region_Test_Strict`, function () {
+
+                cy.visit('https://demo.applitools.com/app.html');
+
+                // Call Open on eyes to initialize a test session
+                cy.eyesOpen({
+                    appName: 'Demo App'
+                })
+
+                // check the login page with fluent api, see more info here
+                // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
+                cy.eyesCheckWindow({
+                    tag: "header",
+                    target: 'region',
+                    selector: {
+                    type: 'css',
+                    selector:'.element-wrapper.compact.pt-4'
+                    }
+                });
+
+                // Call Close on eyes to let the server know it should display the results
+                cy.eyesClose()
+            });
+
+
+
 });
