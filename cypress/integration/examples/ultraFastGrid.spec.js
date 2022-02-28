63de62144acdfe102b41a820e17f7b233778b320
Poonam Dahiya <poonam.dahiya@applitools.com>
2022-03-01 10:55:21 +1100

    test

--- cypress/integration/examples/ultraFastGrid.spec.js
+++ cypress/integration/examples/ultraFastGrid.spec.js
@@ -12,8 +12,7 @@ describe("AppTestNew", () => {
                appName: 'Baseline Demo'
            })
 
-           // check the login page with fluent api, see more info here
-           // https://applitools.com/docs/topics/sdk/the-eyes-sdk-check-fluent-api.html
+
            cy.eyesCheckWindow({
                tag: "Login Window",
                target: 'window',
