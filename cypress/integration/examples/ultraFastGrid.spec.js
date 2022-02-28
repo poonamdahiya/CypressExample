c7ce6c330e11d5114a7b221730ce80e61bd2f60c
Poonam Dahiya <poonam.dahiya@applitools.com>
2022-03-01 10:52:16 +1100

    test

--- cypress/integration/examples/ultraFastGrid.spec.js
+++ cypress/integration/examples/ultraFastGrid.spec.js
@@ -5,7 +5,7 @@ describe("AppTestNew", () => {
            // ⭐️ Note to see visual bugs, run the test using the above URL for the 1st run.
            // but then change the above URL to https://demo.applitools.com/index_v2.html
            // (for the 2nd run)
-           cy.visit('https://demo.applitools.com/index_v2.html');
+           cy.visit('https://demo.applitools.com/');
 
            // Call Open on eyes to initialize a test session
            cy.eyesOpen({
